f4565f095177f96c9bd24bd6b7c526abaa32b1c8
aaronsifuentes47 <sifuentes.aaronm@gmail.com>
2019-08-14 15:19:38 -0500

    statements mysql

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,8 +1,11 @@
 SELECT 'albums after 1991' AS '';
 SELECT * FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
 
 SELECT 'disco genres' AS '';
 SELECT * FROM albums WHERE genre = "disco";
+DELETE FROM albums WHERE genre = "disco";
 
 SELECT '' AS 'albums by Whitney Houston';
 SELECT * FROM albums WHERE artist = "whitney houston";
+DELETE FROM albums WHERE artist = "whitney houston";
