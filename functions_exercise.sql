2b7ff4775883117c081edde3e841fd0c1f1afe6a
aaronsifuentes47 <sifuentes.aaronm@gmail.com>
2019-08-15 15:42:35 -0500

    functions exercise

--- functions_exercise.sql
+++ functions_exercise.sql
@@ -25,11 +25,12 @@ SELECT * FROM employees WHERE last_name LIKE '%q%';
 SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE'%e';
 
 # Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
-SELECT * FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE'%e';
+SELECT concat(first_name, ' ', last_name) AS 'full name' FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE'%e';
 
 # Find all employees hired in the 90s and born on Christmas — 362 rows.
-SELECT * FROM employees WHERE hire_date LIKE '199%' AND (birth_date LIKE '%12-25')
-ORDER BY birth_date, hire_date DESC LIMIT 5 OFFSET 45;
+SELECT *, datediff(NOW(),hire_date) AS 'days since hire' FROM employees WHERE hire_date LIKE '199%' AND (birth_date LIKE '%12-25')
+ORDER BY birth_date, hire_date DESC;
 
 # Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+
--- limit_exercises.sql
+++ limit_exercises.sql
@@ -2,3 +2,6 @@ USE employees;
 
 SELECT DISTINCT last_name FROM employees
 ORDER BY last_name DESC LIMIT 10;
+
+select concat_ws(' ',first_name, last_name) from employees;
+
