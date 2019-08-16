4f5d302c7d4dce64d281a8cd919b852568104366
aaronsifuentes47 <sifuentes.aaronm@gmail.com>
2019-08-16 16:19:15 -0500

    subqueries exercise

---
+++ subqueries_exercises.sql
@@ -0,0 +1,20 @@
+use employees;
+
+select *
+from employees
+where hire_date = (
+    select hire_date from employees where emp_no = 101010
+    );
+
+select title, count(title)
+from titles t
+    join employees e
+        on t.emp_no = e.emp_no
+where e.first_name = 'Aamod'
+group by title
+
+select first_name, last_name
+from dept_manager
+         join departments d on dept_manager.dept_no = d.dept_no
+         join employees e on dept_manager.emp_no = e.emp_no
+where to_date like '999%' and e.gender = 'F';
